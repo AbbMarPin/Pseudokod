c1ba57e8752eb90b4934f24b7ab7d6f227407703
AbbMarPin <54662893+AbbMarPin@users.noreply.github.com>
2020-03-02 13:17:17 +0100

    Javascript

---
+++ Bengts_tonfisk.js
@@ -0,0 +1,5 @@
+let var1 = 347/30
+let var2 = 347%30
+let var3 = var2/13.95
+
+console.log(var1 + var3, "burkar")
---
+++ Åsas_äventyr.js
@@ -0,0 +1,6 @@
+
+console.log("Åsa kan gå igenom Lustiga Huset", Math.round((20 / 2) - 0.5), "gånger")
+
+console.log("Åsa kan åka Galna Musen", Math.round((20 / 3) - 0.5), "gånger")
+
+console.log("Åsa kan Åka den Flygande Kvasten", Math.round((20 / 4) - 0.5), "gånger")
