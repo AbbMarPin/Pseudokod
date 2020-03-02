b00a1638559f2727c54549d9fce04c97419df35a
AbbMarPin <54662893+AbbMarPin@users.noreply.github.com>
2020-03-02 13:07:06 +0100

    Kod

---
+++ Bengts_tonfisk.py
@@ -0,0 +1,5 @@
+var1 = 347/30
+var2 = 347%30
+var3 = var2/13.95
+
+print(var1 + var3, "burkar")
---
+++ Åsas_äventyr.py
@@ -0,0 +1,6 @@
+
+print("Åsa kan gå igenom Lustiga Huset", round((20/2) - 0.5), "gånger")
+
+print("Åsa kan åka Galna Musen", round((20/3) - 0.5), "gånger")
+
+print("Åsa kan Åka den Flygande Kvasten", round((20/4) - 0.5), "gånger")
